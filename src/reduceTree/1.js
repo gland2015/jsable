53867cc79036deac6c9b42c7feb74cf33f5a524a
gland <1147940842@qq.com>
2021-09-09 23:07:18 +0800

    44

--- src/reduceTree/1.js
+++ src/reduceTree/1.js
@@ -1,2 +1,19 @@
+let it = new TreeIterator(tree, childKey);
+it.get(path);
 
+const fn = (item, data, path) => {
+  return data;
+};
+it.iteratDown(fn, initData);
 
+const fn = (item, listData, path) => {
+  return data;
+};
+it.iteraUp(fn);
+
+const fn = (item, { key }, path) => {};
+it.data("key")
+  .someChild((item) => item.a, { includeSelf: false, includeSun: true })
+  .collect(fn);
+
+  

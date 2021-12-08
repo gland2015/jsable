a8a4970ca6cac67c27af368e8293ac5bda975d51
gland <1147940842@qq.com>
2021-12-08 22:09:50 +0800

    33

---
+++ test/index.js
@@ -0,0 +1,19 @@
+import fs from "fs";
+import child_process from "child_process";
+import encoding from "encoding";
+
+// child_process.exec(
+//   "test.bat",
+//   { encoding: "buffer" },
+//   function (error, stdout, stderr) {
+//     let hex = stdout.toString("hex");
+//     console.log(hex);
+
+//     console.log(stdout.toString());
+//   }
+// );
+
+let b = fs.readFileSync("./1").toString();
+
+
+console.log(encoding.convert(b, "cp936", "utf8").toString("hex"));;
